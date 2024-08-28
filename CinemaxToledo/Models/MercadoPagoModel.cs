cc79d6af76ebbcef592c936a908646e8c41315d4
Felipe Sanches <felipesteam2010@gmail.com>
2024-08-27 21:17:57 -0300

    commit

---
+++ CinemaxToledo/Models/MercadoPagoModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CinemaxToledo.Models
+{
+    public class MercadoPagoModel
+    {
+
+        public String email { get; set; }
+        public String nome { get; set; }
+        public String cidade { get; set; }
+        public String estado { get; set; }
+        public String telefone { get; set; }
+        public int idPagamento { get; set; }
+        public String logradouro { get; set; }
+        public String numero { get; set; }
+        public String cep { get; set; }
+        public String nomePlano { get; set; }
+        public decimal valor { get; set; }
+
+    }
+}
